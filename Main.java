f53129f5abc5b76da7a1030f0e31beb507cc231d
TronoKurelz <tarunkurella@gmail.com>
2019-11-30 07:38:22 +0000

    final

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+import java.io.*;
 import java.math.*;
 
 
@@ -10,6 +10,16 @@ static String mutate(String old,int k,char b){
   StringBuffer temp = new StringBuffer(old);
   temp.setCharAt(k,b);
   return temp.toString();
+}
+
+static void cross(ArrayList<String> genes,int d1,int d2,int k1,int k2){
+String dna1 = genes.get(d1);
+String dna2 = genes.get(d2);
+
+String ans1 = dna1.substring(0,k1-1)+dna2.substring(k2-1);
+String ans2 = dna2.substring(0,k2-1)+dna1.substring(k1);
+genes.add(ans1);
+genes.add(ans2);
 }
 
 static void count(char[] strand,int start,int end){
@@ -26,7 +36,8 @@ static void count(char[] strand,int start,int end){
 
 }
  
-  public static  void main(String[] args) {
+  public static  void main(String[] args)throws IOException {
+    try{
   Scanner hello = new Scanner(System.in);
   ArrayList<String> genes = new ArrayList<String>();
   genes.add("000");
@@ -45,18 +56,18 @@ static void count(char[] strand,int start,int end){
       case "MUTATE": int dno=hello.nextInt(); // dna no.
                 int pno=hello.nextInt(); // position no.
                 char b=hello.next().charAt(0); //base to replace 
-                genes.set(dno,mutate(genes.get(dno),pno,b));
+                genes.set(dno,mutate(genes.get(dno),pno-1,b));
                 break;
       case "CROSS" : int dno1=hello.nextInt(); 
                 int dno2=hello.nextInt();
                 int k1=hello.nextInt();
                 int k2=hello.nextInt();
-                System.out.println("called cross for"+dno1+" and offset k2"+k2);
+                cross(genes,dno1,dno2,k1,k2);
                 break;
       case "COUNT" : int dno3=hello.nextInt(); 
                 int k3=hello.nextInt();
                 int k4=hello.nextInt(); 
-                count(genes.get(dno3).toCharArray(),k3,k4);
+                count(genes.get(dno3).toCharArray(),k3-1,k4-1);
 
 
     }
@@ -70,7 +81,9 @@ static void count(char[] strand,int start,int end){
   // ans[0]=(char)((int)ans[0]-32);
   // System.out.println(new String(ans));
 
-}}
+}catch(Exception e){
+			return;
+		}}}
 
 
 
