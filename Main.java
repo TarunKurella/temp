eb2802cf3368348f166833811808726af5f183fc
TronoKurelz <tarunkurella@gmail.com>
2019-12-08 06:38:14 +0000

    hello

--- Main.java
+++ Main.java
@@ -1,67 +1,116 @@
 import java.util.*;
 import java.io.*;
 import java.math.*;
+import java.lang.*;
 
 
 public class Main {
 
-static int cn = 1;
-static void median(int[] problems,int start,int end){
-  int temp[]= Arrays.copyOfRange(problems, start, end+1);
-   Arrays.sort(temp);
-   
-  int n = end-start+1;
-  if((n%2)!=0){
-  n=(n+1)/2;
-  System.out.println(temp[n-1]);
-  }
-  else{
-    System.out.println((temp[(n/2)-1]+temp[((n+2)/2)-1])/2);
-  }
-}
- 
 
 
 
   public static  void main(String[] args)throws IOException {
     try{
-  Scanner hello = new Scanner(System.in);
-  while (hello.hasNextInt()) {
-    System.out.println("Case "+cn+":");
-    int tnp = hello.nextInt();//total no.of problems
-    int[] problems = new int[tnp];
-    for(int i=0;i<tnp;i++){
-      problems[i]=hello.nextInt();
-    }
-    int nq= hello.nextInt(); // n0 of queries
-    for(int j=0;j<nq;j++){
-      int k1=hello.nextInt();
-      int k2=hello.nextInt();
-      median(problems,k1-1,k2-1);
-    }
-cn++;
-    
-}
-  
-
-
-  // int n = hello.nextInt();
-
-  // for(int i=0;i<n;i++){
-    
-
-  //   }
-    
-   
 
  
+  Scanner hello = new Scanner(System.in);
+
+
+
+
+ int ne = hello.nextInt();
+ boolean[] status = {false,false,false};
+
+next:
+ for(int j=0;j<ne;j++){
+  char[] str = hello.next().toCharArray();
+  boolean ans = true;
+if(str.length==1){
+  if(str[0]=='?'){
+    System.out.print("a");
+    
   }
+  continue next;
+}
+ 
+ for(int i=0;i<str.length-1;i++){
+   if(str[i]=='?'){
+     
+     if(str[i+1]!='?'){
+              Arrays.fill(status, false);
+              if(i>0)status[(int)str[i-1]-97]=true;
+              status[(int)str[i+1]-97]=true;
+              for(int k=0;k<3;k++){
+                if(!status[k]){
+                  str[i]=(char)(97+k);
+                }
+              }
+     }else{
+      if(i>0){
+         if(str[i-1]=='a'){
+         str[i]='b';
+       }else if(str[i-1]=='b'){
+         str[i]='c';
+       }else{
+         str[i]='a';
+       }
+      }else{
+        str[i]='a';
+      }
+       
+
+     }
+     continue;
+   }
+   if(i==0)continue;
+   
+   if((str[i-1]==str[i])||(str[i]==str[i+1]))
+   {
+     ans = false;
+     break;
+   }
+ }
+ int te = str.length-1;
+ if(str[te]=='?'){
+   if(str[te-1]=='a'){
+         str[te]='b';
+       }else if(str[te-1]=='b'){
+         str[te]='c';
+       }else{
+         str[te]='a';
+       }
+  
+}
+
+ if(ans){
+   System.out.println(new String(str));
+
+ }else{
+   System.out.println("-1");
+ }
+
+
+ 
+ }
+
   
 
-  // if(!(ans[0]>=65&&ans[0]<=90))
-  // ans[0]=(char)((int)ans[0]-32);
-  // System.out.println(new String(ans));
 
+//  int ni = hello.nextInt();
+ 
+
+ 
+//  for(int i=0;i<ni;i++){
+
+    
+
+ 
+ 
+// // //   // if(!(ans[0]>=65&&ans[0]<=90))
+// // //   // ans[0]=(char)((int)ans[0]-32);
+// // //   // System.out.println(new String(ans));
+//   }
+}
 catch(Exception e){
 			return;
 		}}}
